b74983a793bfb07be58e77ac2ee41b4d6f5c2062
Pranav AVN <pranavavn0811@gmail.com>
2024-03-13 19:39:35 +0530

    Code refactor

--- fourbitadder.v
+++ fourbitadder.v
@@ -33,3 +33,53 @@ module four_bit_adder(S,C4,A,V,C0);
         FA2(S[2], C3, A[2], B[2], C2),
         FA3(S[3], C4, A[3], B[3], C3);
 endmodule
+
+module testbench();
+    //Inputs
+    reg[3:0]a;
+    reg[3:0]b;
+    reg cin;
+    //Outputs
+    wire[3:0]sum;
+    wire cout;
+
+    four_bit_adder DUT(
+        .A(a),
+        .B(b),
+        .C0(cin),
+        .S(sum),
+        .C4(cout)
+    );
+    reg clk;
+    reg rst;
+
+    //Initial values
+    initial begin
+        clk = 0;
+        rst = 1;
+        #20;
+        rst = 0;
+    end
+
+    //Clock
+    always begin
+        #10 clk = ~clk;
+    end
+
+    //Test Sequence
+    reg[3:0]i;
+    always @(posedge clk, posedge rst) begin
+        if(rst)begin
+            i=0;
+        end else begin
+            //Apply all possible values(0-9)
+            a<=$random%16;
+            b<=$random%16;
+            cin<=$random%2;
+            #20;
+            i = i+1;
+            if(i == 10) $finish;
+        end
+        $monitor("%d: a= %b, b=%b, cin=%b, sum=%b, cout=%b",$time, a, b, cin, sum, cout);
+    end
+endmodule
